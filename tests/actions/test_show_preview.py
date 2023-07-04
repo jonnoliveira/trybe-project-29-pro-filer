e5cb9674eaab372bb605f22cb1dc9a6e07b07547
jonnoliveira <jonnutri@gmail.com>
2023-07-04 18:11:44 -0300

    Fix: show_preview test implementation - req03

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -8,8 +8,9 @@ call_one = [
 
 call_two = [
     {"all_files": ["file1"], "all_dirs": []},
-    """Found 1 files and 0 directories\nFirst 5 files: ['file1']\nFirst 5
-directories: []\n""",
+    "Found 1 files and 0 directories\n\
+     First 5 files: ['file1']\n\
+     First 5 directories: []\n",
 ]
 
 
