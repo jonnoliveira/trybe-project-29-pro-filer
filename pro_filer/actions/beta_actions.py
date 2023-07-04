65e9e766979e80da8deddcd37382d89a56ba1313
jonnoliveira <jonnutri@gmail.com>
2023-07-04 15:49:23 -0300

    Add: show_deepest_file implementation - req01

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,11 +1,19 @@
 """Arquivo que estudantes devem editar"""
+import os
 
 
 def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        """A função 'lambda' é uma função anônima como as arrow
+        functions em JS. Nesse caso ela recebe um caminho da
+        lista de arquivos e retorna o tamanho da lista de diretórios que
+        compõem o caminho. O 'max' vai retornar o caminho que tiver
+        o maior tamanho."""
+        deepest_file = max(
+            context["all_files"], key=lambda path: len(os.path.split(path))
+        )
         print(f"Deepest file: {deepest_file}")
 
 
