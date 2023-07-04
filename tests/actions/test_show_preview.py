845b978494d8eda68d7c747c37e4428ae66e5cce
jonnoliveira <jonnutri@gmail.com>
2023-07-04 17:18:35 -0300

    Add: show_preview test implementation - req03

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,19 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+import pytest
+
+call_one = [
+    {"all_files": [], "all_dirs": []},
+    "Found 0 files and 0 directories",
+]
+
+
+@pytest.mark.parametrize(
+    "context, expected_output",
+    [
+        (call_one[0], call_one[1]),
+    ],
+)
+def test_show_preview(context, expected_output, capsys):
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == expected_output
