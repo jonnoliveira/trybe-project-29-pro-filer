77fd4b1e954f5c9502c2565bfc32c89af43884ad
jonnoliveira <jonnutri@gmail.com>
2023-07-04 19:08:49 -0300

    Fix: show_preview test implementation - req03

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,19 +1,29 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+import pytest
 
 call = [
     {"all_files": ["file"], "all_dirs": []},
-    [
-        "Found 1 files and 0 directories\n",
-        "First 5 files: ['file']\n",
-        "First 5 directories: []\n",
-    ],
+    "Found 1 files and 0 directories\n",
+    "First 5 files: ['file']\n",
+    "First 5 directories: []\n",
 ]
 
 
-def test_show_preview(capsys):
-    show_preview(call)
+@pytest.mark.parametrize(
+    "context, expected_line_1, expected_line_3, expected_line_3",
+    [
+        (call[0], call[1]),
+    ],
+)
+def test_show_preview(
+    context, expected_line_1, expected_line_2, expected_line_3, capsys
+):
+    show_preview(context)
     captured = capsys.readouterr()
-    assert call[1][0] + call[1][1] + call[1][2] in captured.out
+    for line1, line2, line3 in captured.out.splitlines():
+        assert line1 == expected_line_1
+        assert line2 == expected_line_2
+        assert line3 == expected_line_3
 
 
 def test_show_preview_empty(capsys):
