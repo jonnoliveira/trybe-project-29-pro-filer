0b3119e51e0f863b656639b8d14348c0a346fca4
jonnoliveira <jonnutri@gmail.com>
2023-07-04 23:01:37 -0300

    Add: show_disk_usage implementation - req05

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,34 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+from pro_filer.cli_helpers import _get_printable_file_path  # NOQA
+
+
+def test_show_disk_usage(tmp_path, capsys):
+    # cria arquivos temporarios
+    path_one = tmp_path / "file.txt"
+    path_one.touch()
+
+    path_two = tmp_path / "file2.txt"
+    path_two.touch()
+    # escreve nesses arquivos
+    path_one.write_text("Shinigami like apple")
+    path_two.write_text("Shinigami can see your death date")
+
+    context = {"all_files": [str(path_one), str(path_two)]}
+    output_one = f"'{_get_printable_file_path(str(path_one))}':".ljust(70)
+    output_two = f"'{_get_printable_file_path(str(path_two))}':".ljust(70)
+
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == f"{output_two} 33 (62%)\n{output_one} 20 (37%)\nTotal size: 53\n"
+    )
+
+
+def test_show_disk_usage_empty(capsys):
+    context = {"all_files": []}
+    output = "Total size: 0\n"
+
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+    assert output in captured.out
